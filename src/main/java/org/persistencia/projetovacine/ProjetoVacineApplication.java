e7dd34aa85b0fefbe20fda0736788422bfb27f7c
lucas-bernardes03 <lucasbernardes24@gmail.com>
2024-11-12 20:59:15 -0300

    inital commit

---
+++ src/main/java/org/persistencia/projetovacine/ProjetoVacineApplication.java
@@ -0,0 +1,13 @@
+package org.persistencia.projetovacine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVacineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoVacineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/persistencia/projetovacine/ProjetoVacineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.persistencia.projetovacine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVacineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
