730edb616850a7a9d87e7fc4265e32bd7e957e6a
Lucas Bernardes <lucasbernardes24@gmail.com>
2024-12-03 16:19:58 -0300

    .

--- frontend/src/app/pages/usuarios/usuarios.component.ts
+++ frontend/src/app/pages/usuarios/usuarios.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Button} from 'primeng/button';
 import {DatePipe, NgIf} from '@angular/common';
-import {PrimeTemplate} from 'primeng/api';
+import {ConfirmationService, MessageService, PrimeTemplate} from 'primeng/api';
 import {TableModule} from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
 import {UsuarioService} from '../../service/usuario.service';
 import {Usuario} from '../../interface/usuario';
+import {DialogService, DynamicDialogRef} from 'primeng/dynamicdialog';
+import {EditarUsuarioComponent} from './editar-usuario/editar-usuario.component';
 
 @Component({
   selector: 'app-usuarios',
@@ -24,12 +26,82 @@ import {Usuario} from '../../interface/usuario';
 export class UsuariosComponent implements OnInit{
   usuarios!: Usuario[];
 
-  constructor(private usuarioService: UsuarioService) {
+  ref: DynamicDialogRef | undefined
+
+  constructor(private usuarioService: UsuarioService,
+              private dialogService : DialogService,
+              private messageService: MessageService,
+              private confirmationService : ConfirmationService) {
   }
 
   ngOnInit() {
+    this.listarUsuarios()
+  }
+
+  adicionar(){
+    this.ref = this.dialogService.open(EditarUsuarioComponent, {
+      header: 'Novo Usuário',
+      width: '20vw',
+      height: '65vh'
+    })
+
+    this.ref.onClose.subscribe(result => {
+      if(result){
+        this.messageService.add({severity: 'success', summary: 'Sucesso', detail: 'Novo usuário cadastrado com sucesso!'})
+        this.listarUsuarios()
+      }
+    })
+  }
+
+  editar(usuario: Usuario){
+    this.ref = this.dialogService.open(EditarUsuarioComponent, {
+      data: usuario,
+      header: 'Editar Usuario',
+      width: '20vw',
+      height: '65vh'
+    })
+
+    this.ref.onClose.subscribe(result => {
+      if(result){
+        this.messageService.add({severity: 'success', summary: 'Sucesso', detail: 'Usuario alterado com sucesso!'})
+        this.listarUsuarios()
+      }
+    })
+  }
+
+  excluir(usuario: Usuario, event : Event){
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Tem certeza que deseja excluir o registro?',
+      header: 'Confirmação',
+      icon: 'pi pi-exclamation-triangle',
+      rejectButtonStyleClass:"p-button-text",
+      acceptLabel: 'Sim',
+      rejectLabel: 'Não',
+      accept: () => {
+        this.usuarioService.excluirUsuario(usuario).subscribe({
+          next: () => {
+            this.messageService.add({severity: 'success', summary: 'Sucesso', detail: 'Usuario excluído com sucesso!'})
+            this.listarUsuarios()
+          },
+          error: () => {
+            this.messageService.add({severity: 'error', summary: 'Erro', detail: 'Erro ao excluir usuario'})
+          }
+        })
+      },
+      reject: () => {
+      }
+    })
+  }
+
+  listarUsuarios(){
     this.usuarioService.getUsuarios().subscribe({
-      next: result => this.usuarios = result
+      next: result => {
+        this.usuarios = result
+      },
+      error: () => {
+        this.messageService.add({severity: 'error', summary: 'Erro', detail: 'Erro ao listar usuariomentos'})
+      }
     })
   }
 }
--- frontend/src/app/service/usuario.service.ts
+++ frontend/src/app/service/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {HOST_URL} from './agenda.service';
 import {Observable} from 'rxjs';
+import {Usuario} from '../interface/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,15 @@ export class UsuarioService {
     return this.http.get(HOST_URL + '/usuario')
   }
 
+  novaUsuario(usuario: Usuario): Observable<any>{
+    return this.http.post(HOST_URL + '/usuario', usuario)
+  }
+
+  salvarUsuario(usuario: Usuario) : Observable<any>{
+    return this.http.put(HOST_URL + '/usuario', usuario)
+  }
+
+  excluirUsuario(usuario : Usuario): Observable<any>{
+    return this.http.delete(HOST_URL + '/usuario', {body: usuario})
+  }
 }
